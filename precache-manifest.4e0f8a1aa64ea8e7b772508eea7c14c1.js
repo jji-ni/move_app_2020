1a74c248ee10ab87ba1cb2e5a55e7eb2b3da5dd9
조진주 <jojinju@jojinjuui-MacBook-Pro.local>
2020-09-23 16:26:00 +0900

    Updates

---
+++ precache-manifest.4e0f8a1aa64ea8e7b772508eea7c14c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9792336ccc771b23e18efbf446c3bfd",
+    "url": "/move_app_2020/index.html"
+  },
+  {
+    "revision": "6186dad3c71235bde35f",
+    "url": "/move_app_2020/static/css/main.1764592e.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/move_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/move_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6186dad3c71235bde35f",
+    "url": "/move_app_2020/static/js/main.f0d774df.chunk.js"
+  },
+  {
+    "revision": "96757a65436598d2bba9",
+    "url": "/move_app_2020/static/js/runtime-main.8e6723b9.js"
+  }
+]);
